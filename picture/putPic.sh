5bbfc9f98d45d78ff95b2fcc6694cbe2a74f19f6
hujw <jianwei.hu@bangcle.com>
2019-09-11 11:07:37 +0800

    put picture

---
+++ picture/putPic.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+git add .
+sleep 1s
+git commit -m "put picture"
+sleep 1s
+git push origin master
+echo "https://github.com/hujwGitHub2017/mypicture/blob/master/picture/***.png?raw=true"
